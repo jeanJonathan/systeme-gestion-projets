207c8da130b0db80c1d12e44f9b098c2aaf54fc1
Jean Jonathan <jeanjonathank2@gmail.com>
2023-03-06 15:25:39 +0100

    Creation et implementation de la migration projets pour gerer la table projets de la base de donnee

---
+++ database/migrations/2023_03_06_141126_create_projets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //Blueprint est une methode de constructeur de schema pour permettre
+        //L'ajout de nouvelle colone s
+        Schema::create('projets', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('description');
+            $table->date('date_debut');
+            $table->date('date_fin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projets');
+    }
+};
