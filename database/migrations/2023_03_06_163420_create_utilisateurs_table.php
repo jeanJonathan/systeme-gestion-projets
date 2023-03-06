5f07962874ab6820d103eff8b9419b99e8f6a76b
Jean Jonathan <jeanjonathank2@gmail.com>
2023-03-06 17:37:00 +0100

    Creation et implementation de la migration utilisateurs pour gerer la table utilisateurs de la base de donnee

--- database/migrations/2023_03_06_163420_create_utilisateurs_table.php
+++ database/migrations/2023_03_06_163420_create_utilisateurs_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('utilisateurs', function (Blueprint $table) {
             $table->id();
+            $table->string('nom');
+            $table->string('email');
+            $table->string('password');
             $table->timestamps();
         });
     }
