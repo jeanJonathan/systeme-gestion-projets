ea999d8cb1bb8dceab85d04b369fedbdc007cab5
Jean Jonathan <jeanjonathank2@gmail.com>
2023-03-22 12:22:00 +0100

    Ajout de la fonction index pour afficher tous les projets

--- app/Http/Controllers/ControleurProjet.php
+++ app/Http/Controllers/ControleurProjet.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+//Pour utilise le modele Projet pour acceder a la
+//table des projets dans la base de donnees
+use App\Models\Projet;
 use Illuminate\Http\Request;
 
 class ControleurProjet extends Controller
 {
-    //
+    /**
+     * Affiche la liste de tous les projets.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projets = Projet::all();
+
+        return view('projets.index', compact('projets'));
+    }
+
 }
